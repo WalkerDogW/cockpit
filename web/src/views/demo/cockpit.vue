b61ecb89371deec93fcc11ffcc5f73da6f5823e8
WalkerDogW <WalkerDogW@outlook.com>
2020-08-28 20:36:18 +0800

    Initial commit

---
+++ web/src/views/demo/cockpit.vue
@@ -0,0 +1,48 @@
+<template>
+<dv-full-screen-container>
+    <!-- <dv-loading>Loading...</dv-loading>  -->
+    <dv-border-box-1>dv-border-box-1</dv-border-box-1>
+    content2</dv-full-screen-container>
+</template>
+
+<script>
+export default {
+//import 引入的组件需要注入到对象中才能使用
+components:{},
+props:{},
+data(){
+//这里存数据
+return{};
+},
+//计算属性
+computed: {
+
+},
+//监控data中数据变化
+watch: {
+
+},
+//方法
+methods: {
+
+},
+//声明周期 - 创建完成（可以访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {
+
+},
+beforeCreate() {},//生命周期 - 创建之前
+beforeMount() {},//生命周期 - 挂载之前
+beforeUpdate() {},//声明周期 - 更新之前
+updated() {},//生命周期 - 更新之后
+beforeDestroy() {},//生命周期 - 销毁之前
+destroyed() {},//生命周期 - 销毁之后
+activated() {},//缓存keep-alive
+};
+</script>
+
+<style scoped>
+</style>
