2b690e18673f66a45bc511795956e4ec526d783f
WalkerDogW <WalkerDogW@outlook.com>
2020-08-29 08:11:19 +0800

    topHeader

--- web/src/views/demo/cockpit.vue
+++ web/src/views/demo/cockpit.vue
@@ -1,48 +1,78 @@
 <template>
-<dv-full-screen-container>
-    <!-- <dv-loading>Loading...</dv-loading>  -->
-    <dv-border-box-1>dv-border-box-1</dv-border-box-1>
-    content2</dv-full-screen-container>
+  <div id="data-view">
+    <dv-full-screen-container>
+         <top-header />
+    </dv-full-screen-container>
+  </div>
 </template>
 
 <script>
+import topHeader from './topHeader'
 export default {
-//import 引入的组件需要注入到对象中才能使用
-components:{},
-props:{},
-data(){
-//这里存数据
-return{};
-},
-//计算属性
-computed: {
-
-},
-//监控data中数据变化
-watch: {
-
-},
-//方法
-methods: {
-
-},
-//声明周期 - 创建完成（可以访问当前this实例）
-created() {
-
-},
-//生命周期 - 挂载完成（可以访问DOM元素）
-mounted() {
-
-},
-beforeCreate() {},//生命周期 - 创建之前
-beforeMount() {},//生命周期 - 挂载之前
-beforeUpdate() {},//声明周期 - 更新之前
-updated() {},//生命周期 - 更新之后
-beforeDestroy() {},//生命周期 - 销毁之前
-destroyed() {},//生命周期 - 销毁之后
-activated() {},//缓存keep-alive
+  //import 引入的组件需要注入到对象中才能使用
+  components: {topHeader,},
+  props: {},
+  data() {
+    //这里存数据
+    return {};
+  },
+  //计算属性
+  computed: {},
+  //监控data中数据变化
+  watch: {},
+  //方法
+  methods: {},
+  //声明周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //声明周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁之后
+  activated() {}, //缓存keep-alive
 };
 </script>
 
 <style scoped>
+#data-view {
+  width: 100%;
+  height: 100%;
+  background-color: #030409;
+  color: #fff;
+}
+#data-view #dv-full-screen-container {
+  background-image: url('/static/img/background02.jpg');
+  background-size: 100% 100%;
+  box-shadow: 0 0 3px blue;
+  display: flex;
+  flex-direction: column;
+}
+/* #data-view .main-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+#data-view .block-left-right-content {
+  flex: 1;
+  display: flex;
+  margin-top: 20px;
+}
+#data-view .block-top-bottom-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding-left: 20px;
+}
+#data-view .block-top-content {
+  height: 55%;
+  display: flex;
+  flex-grow: 0;
+  box-sizing: border-box;
+  padding-bottom: 20px;
+} */
+
 </style>
